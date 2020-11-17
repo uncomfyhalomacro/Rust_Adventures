3fe7ed78fe9ffb22cea7221bfabfd385dd4f33a6
ReneganRonin <66054069+ReneganRonin@users.noreply.github.com>
2020-11-17 20:26:49 +0800

    Adding new folders related to learnings

---
+++ scopes/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!");
+
+    println!("{}", s);
+
+    let x = 5;
+    let y = x;
+    
+    println!("x:{}, y:{}", x, y);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s2: {}, s1: {}", s2, s1);
+}
+
+
