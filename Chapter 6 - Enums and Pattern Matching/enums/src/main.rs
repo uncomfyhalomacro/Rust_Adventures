8007ccff721a852464937d2b39129d6bf4153e4b
Renegan Ronin <66054069+ReneganRonin@users.noreply.github.com>
2020-12-26 17:04:11 +0800

    Completed Chapter 6

---
+++ Chapter 6 - Enums and Pattern Matching/enums/src/main.rs
@@ -0,0 +1,40 @@
+use std::{io, println};
+
+use io::Result;
+
+fn main() {
+    // let some_number = Some(5);
+    // let some_string = Some("A string");
+    // let absent_number: Option<i32> = None;
+    //
+    let x: i8 = 5;
+    let mut input = String::new();
+    println!("Input a number you wanna add:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to readline");
+
+    let input: Option<i8> = match input.trim().parse() {
+        Ok(num) => Some(num),
+        Err(_) => None,
+    };
+
+    // if add(x, input).unwrap() == None {
+    //     println!("You have not inputted any value!!")
+    // } else {
+    //     println!("Result: {}", add(x, input).unwrap().unwrap());
+    // }
+
+    if let Some(y) = input {
+        println!("Result: {}", x + y);
+    } else {
+        println!("You have entered nothing or an invalid input!");
+    }
+}
+
+// fn add(x: i8, num: Option<i8>) -> Result<Option<i8>> {
+//     match num {
+//         Some(i) => Ok(Some(i + x)),
+//         None => Ok(None),
+//     }
+// }
