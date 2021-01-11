f3a41fac3777612aecad82754d94455ea9b15267
Renegan Ronin <66054069+ReneganRonin@users.noreply.github.com>
2021-01-11 10:50:27 +0800

    Adding Chapter 7

---
+++ Chapter 7 - Managing Growing Projects with Packages, Crates, and Modules/restaurant/src/front_of_house.rs
@@ -0,0 +1,3 @@
+pub mod hosting;
+
+mod serving;
--- Chapter 7 - Managing Growing Projects with Packages, Crates, and Modules/restaurant/src/lib.rs
+++ Chapter 7 - Managing Growing Projects with Packages, Crates, and Modules/restaurant/src/lib.rs
@@ -6,58 +6,25 @@
 //     }
 // }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
+mod back_of_house;
 fn serve_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
-    }
-    fn cook_order() {}
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
-
 // Using "use" to bring the modules into the scope
-use crate::front_of_house::hosting;
+// use crate::front_of_house::hosting;
+
+// Optionally, you can also use relative paths
+// use self::front_of_house::hosting;
+
+// To bring the names into scope as public, we can combine "pub" and "use"
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    //crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
 
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
