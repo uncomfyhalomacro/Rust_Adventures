51a9fc2aa2fc0c143f3cc650b132f2a9b6374202
Renegan Ronin <66054069+ReneganRonin@users.noreply.github.com>
2021-01-18 15:45:42 +0800

    Updated some exercises from Chapter 8

--- Chapter 8 - Common Collections/pig-latin/src/main.rs
+++ Chapter 8 - Common Collections/pig-latin/src/main.rs
@@ -5,41 +5,38 @@ fn main() {
     // let mut user_input = String::from("vestergurkan");
     println!("Enter a word:");
     loop {
-        io::stdin()
-            .read_line(&mut user_input)
-            .expect("Failed to read line");
-        let mut user_input = user_input.trim().to_string();
-        if user_input.is_empty() {
+        let word = input(&mut user_input);
+        if word.is_empty() {
             println!("Empty input! If you want to exit, press Ctrl+c.\nOtherwise, Try again:");
         } else {
-            let pig = pig_latin(&mut user_input);
+            let pig = pig_latin(word);
             println!("Pigged: {}", pig);
             break;
         }
     }
 }
 
-fn pig_latin(s: &mut String) -> String {
+fn pig_latin(s: &str) -> String {
     let vowels = String::from("aeiou");
     let first_letter: char = s.chars().next().unwrap_or_default();
-    let mut vector_chars: Vec<char> = Vec::new();
-    for char in s.chars() {
-        vector_chars.push(char);
-    }
     if vowels.contains(first_letter) {
-        s.push_str("hay");
-        s.to_string()
+        [s, "hay"].concat()
     } else {
-        let mut new_str = s
+        let new_str: &str = s
             .char_indices()
             .nth(1)
-            .and_then(|(i, _)| s.get(i..))
-            .unwrap_or("")
-            .to_string();
-        new_str.push('-');
-        let mut new_substr: String = vector_chars[0].to_string();
-        new_substr.push_str("ay");
-        new_str.push_str(&new_substr);
-        new_str
+            .and_then(move |(i, _)| s.get(i..))
+            .unwrap_or("");
+
+        [new_str, &["-", &first_letter.to_string(), "ay"].concat()].concat()
     }
+}
+
+fn input(user_input: &mut String) -> &str {
+    io::stdin()
+        .read_line(user_input)
+        .expect("Failed to read line");
+
+    let user_input = user_input.trim();
+    user_input
 }
